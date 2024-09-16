fe179c6caac2b919947fa153935a7b8dc54505ec
José <josebeto1113@gmail.com>
2024-09-15 21:40:12 -0300

    Feat: Tratando exceções da entidade Category

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -14,17 +14,31 @@ namespace AspBlog.Controllers
             [FromServices] DataContext ctx
         )
         {
-            var category = await ctx.Categories.FirstOrDefaultAsync(x => x.Id == id);
-            if (category == null)
-                return NotFound();
-            return Ok(category);
+            try
+            {
+                var category = await ctx.Categories.FirstOrDefaultAsync(x => x.Id == id);
+                if (category == null)
+                    return NotFound();
+                return Ok(category);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, "Houve um erro interno no servidor");
+            }
         }
 
         [HttpGet("v1/categories")]
         public async Task<IActionResult> ListAsync([FromServices] DataContext ctx)
         {
-            var categories = await ctx.Categories.AsNoTracking().ToListAsync();
-            return Ok(categories);
+            try
+            {
+                var categories = await ctx.Categories.AsNoTracking().ToListAsync();
+                return Ok(categories);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, "Houve um erro interno no servidor");
+            }
         }
 
         [HttpPost("v1/categories")]
@@ -33,9 +47,20 @@ namespace AspBlog.Controllers
             [FromServices] DataContext ctx
         )
         {
-            await ctx.Categories.AddAsync(category);
-            await ctx.SaveChangesAsync();
-            return Created($"v1/categories/{category.Id}", category);
+            try
+            {
+                await ctx.Categories.AddAsync(category);
+                await ctx.SaveChangesAsync();
+                return Created($"v1/categories/{category.Id}", category);
+            }
+            catch (DbUpdateException e)
+            {
+                return StatusCode(500, "Houve um erro ao tentar salvar os dados no banco");
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, "Houve um erro interno no servidor");
+            }
         }
 
         [HttpPut("v1/categories/{id:int}")]
@@ -45,14 +70,25 @@ namespace AspBlog.Controllers
             [FromServices] DataContext ctx
         )
         {
-            var targetCategory = await ctx.Categories.FirstOrDefaultAsync(x => x.Id == id);
-            if (targetCategory == null)
-                return NotFound();
-            category.Name = targetCategory.Name;
-            category.Slug = targetCategory.Slug;
-            ctx.Categories.Update(category);
-            await ctx.SaveChangesAsync();
-            return Ok(category);
+            try
+            {
+                var targetCategory = await ctx.Categories.FirstOrDefaultAsync(x => x.Id == id);
+                if (targetCategory == null)
+                    return NotFound();
+                targetCategory.Name = category.Name;
+                targetCategory.Slug = category.Slug;
+                ctx.Categories.Update(targetCategory);
+                await ctx.SaveChangesAsync();
+                return Ok(targetCategory);
+            }
+            catch (DbUpdateException e)
+            {
+                return StatusCode(500, "Houve um erro ao tentar salvar os dados no banco");
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, "Houve um erro interno no servidor");
+            }
         }
 
         [HttpDelete("v1/categories/{id:int}")]
@@ -61,12 +97,23 @@ namespace AspBlog.Controllers
             [FromServices] DataContext ctx
         )
         {
-            var category = await ctx.Categories.FirstOrDefaultAsync(x => x.Id == id);
-            if (category == null)
-                return NotFound();
-            ctx.Categories.Remove(category);
-            await ctx.SaveChangesAsync();
-            return Ok(category);
+            try
+            {
+                var category = await ctx.Categories.FirstOrDefaultAsync(x => x.Id == id);
+                if (category == null)
+                    return NotFound();
+                ctx.Categories.Remove(category);
+                await ctx.SaveChangesAsync();
+                return Ok(category);
+            }
+            catch (DbUpdateException e)
+            {
+                return StatusCode(500, "Houve um erro ao tentar apagar os dados do banco");
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, "Houve um erro interno no servidor");
+            }
         }
     }
 }
